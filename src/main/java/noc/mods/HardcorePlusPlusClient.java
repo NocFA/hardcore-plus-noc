7449cc9fe8e49cedbb0092fe0c8562789e20dea0
Adam <Nocturnef4@gmail.com>
2024-09-02 20:21:26 +0100

    Initial commit
    Added fabric template
    
    Took 1 hour 24 minutes

---
+++ src/main/java/noc/mods/HardcorePlusPlusClient.java
@@ -0,0 +1,10 @@
+package noc.mods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HardcorePlusPlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
